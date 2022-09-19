f988042de087911d8a714e5362f6507b7b22da42
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-19 13:53:38 +0530

    Adjacency Matrix

---
+++ Graphs/AdjMatrix.java
@@ -0,0 +1,25 @@
+package Graphs;
+import java.util.Scanner;
+public class AdjMatrix {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        int e = sc.nextInt();
+        int [][] adjMatrix = new int[v][v];
+        for (int i = 0; i < e; i++) {
+            System.out.println("Enter the vertices of the " + i + "th" + "edge");
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+            adjMatrix[v1][v2] = 1;
+            adjMatrix[v2][v1] = 1;
+        }
+        for (int i = 0; i < adjMatrix.length; i++) {
+            for (int j = 0; j < adjMatrix.length; j++) {
+                System.out.println("this is the adjacency matrix");
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
